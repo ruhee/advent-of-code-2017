6941c85e3298fd6cdff2a843bb8e99b82af008cc
Ruhee Dewji <ruhee@twg.io>
2017-12-03 22:53:28 -0500

    Day 2

--- 02/day2.py
+++ 02/day2.py
@@ -2,6 +2,7 @@
 import csv
 
 differences = []
+results = []
 
 def get_difference(row):
   return row[(len(row)-1)] - row[0]
@@ -11,9 +12,17 @@ with open('input.tsv', 'r') as tsvfile:
 
   for row in reader:
     int_row = [int(item) for item in row]
+
+    # part 1
     int_row.sort()
     differences.append(get_difference(int_row))
 
-  print sum(differences)
+    # part 2
+    for idx, val in enumerate(int_row):
+      for i, v in enumerate(int_row):
+        if (val % v == 0 and idx != i):
+          results.append(val/v)
 
+  print sum(differences)
+  print sum(results)
 
