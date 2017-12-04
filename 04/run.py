b8c76b105e82945046b0539a8ecf74a5849b237e
Ruhee Dewji <ruhee@twg.io>
2017-12-04 13:29:32 -0500

    Day 4, part 2

--- 04/run.py
+++ 04/run.py
@@ -1,13 +1,26 @@
 #! /usr/bin/python
-with open('input.txt', 'r') as txtfile:
+with open('test2.txt', 'r') as txtfile:
   data = txtfile.read().strip().split('\n')
 
-valid_phrases = 0
+  valid_phrases = 0
+  valid_anagrams = 0
 
-for line in data:
-  pieces = line.split(' ')
-  unique_pieces = set(pieces)
-  if len(pieces) == len(unique_pieces):
-    valid_phrases += 1
+  for line in data:
+    pieces = line.split(' ')
 
-print valid_phrases
+    # part 1
+    unique_pieces = set(pieces)
+    if len(pieces) == len(unique_pieces):
+      valid_phrases += 1
+
+    # part 2
+    for index, item in enumerate(pieces):
+      pieces[index] = ''.join(sorted(item))
+
+    unique_sorted = set(pieces)
+    if len(pieces) == len(unique_sorted):
+      valid_anagrams += 1
+
+  print valid_phrases
+  print valid_anagrams
+
