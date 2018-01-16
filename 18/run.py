2a19a0116a0c65ca0c66cdab53940f0bb9f0f944
Ruhee Dewji <ruhee@twg.io>
2018-01-16 17:13:38 -0500

    Still more WIP

--- 18/run.py
+++ 18/run.py
@@ -5,64 +5,56 @@
 with open('input.txt', 'r') as txtfile:
   data = txtfile.read().strip().split('\n')
 
+  current_instruction = 0
+  last_played_val = 0
+  last_recovered_val = 0
+
+  # setup empty objects
   registers = {}
   keys = []
 
   for line in data:
-    register = line.split()[1]
-    # instructions can be like `jgz 1 3`, don't use that as a key
-    if not register.isdigit(): # TODO: This might be a negative number
-      keys.append(register)
+    x = line.split()[1]
+    if not x.isdigit():
+      keys.append(x)
 
   keys = sorted(set(keys))
   for k in keys:
     registers[k] = 0
 
-  current_instruction = 0
-  last_played_val = 0
-  last_recovered_val = 0
-
   while last_recovered_val == 0:
-    line = data[current_instruction].split()
-    instruction = line[0]
-    register = line[1] # TODO: This might be a number instead of a register key
+    current_line = data[current_instruction].split()
+    instruction = current_line[0]
+    x = current_line[1]
+    if len(current_line) > 2:
+      y = current_line[2]
+
+    try:
+      y = int(y)
+    except ValueError:
+      y = registers[y]
 
     if instruction == 'snd':
-      last_played_val = registers[register]
+      last_played_val = registers[x]
     elif instruction == 'rcv':
       last_recovered_val = last_played_val
-      pass
-    else:
-      value = line[2]
-
+    elif instruction == 'set':
+      registers[x] = y
+    elif instruction == 'add':
+      registers[x] += y
+    elif instruction == 'mul':
+      registers[x] = registers[x] * y
+    elif instruction == 'mod':
+      registers[x] = registers[x] % y
+    elif instruction == 'jgz':
       try:
-        value = int(value)
+        x = int(x)
       except ValueError:
-        value = registers[value]
-
-      if instruction == 'set':
-        registers[register] = value
-      elif instruction == 'add':
-        registers[register] += value
-      elif instruction == 'mul':
-        registers[register] = registers[register] * value
-      elif instruction == 'mod':
-        registers[register] = registers[register] % value
-      elif instruction == 'jgz':
-        if register.isdigit():
-          if register is not 0:
-            current_instruction = current_instruction + register
-            pass
-          pass
-        else:
-          if registers[register] is not 0:
-            current_instruction = current_instruction + registers[register]
-            pass
-          pass
-        pass
-    pass
+        x = registers[x]
+      if x is not 0:
+        current_instruction += y
 
     if instruction is not 'jgz':
       current_instruction += 1
 
-print last_recovered_val
+  print last_recovered_val
